1a84d6c8866c408102d2ab56659f47037914dff8
Kodik77rus <plyhach@gmail.com>
2022-05-08 00:20:19 +0300

    connect sidebar to store

--- src/components/saidbar/index.tsx
+++ src/components/saidbar/index.tsx
@@ -1,52 +1,100 @@
-export const Sidebar = () => (
-  <>
-    <h2 className='grid-title'>
-      <i className='fa fa-inbox'></i> Mailbox
-    </h2>
-    <a
-      className='btn btn-block btn-primary'
-      data-toggle='modal'
-      data-target='#compose-modal'
-    >
-      <i className='fa fa-pencil'></i>&nbsp;&nbsp;NEW MESSAGE
-    </a>
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { useState } from 'react'
 
-    <hr />
+import { RootState } from '../../store'
+import { SideBarProps } from '../../types'
 
-    <div>
-      <ul className='nav nav-pills nav-stacked' style={{ display: 'block' }}>
-        <li className='header'>Folders</li>
-        <li className='active'>
-          <a href='#'>
-            <i className='fa fa-inbox'></i> Inbox (14)
-          </a>
-        </li>
-        <li>
-          <a href='#'>
-            <i className='fa fa-star'></i> Starred
-          </a>
-        </li>
-        <li>
-          <a href='#'>
-            <i className='fa fa-bookmark'></i> Important
-          </a>
-        </li>
-        <li>
-          <a href='#'>
-            <i className='fa fa-mail-forward'></i> Sent
-          </a>
-        </li>
-        <li>
-          <a href='#'>
-            <i className='fa fa-pencil-square-o'></i> Drafts
-          </a>
-        </li>
-        <li>
-          <a href='#'>
-            <i className='fa fa-folder'></i> Spam (217)
-          </a>
-        </li>
-      </ul>
-    </div>
-  </>
-)
+export const SidebarComponent = ({
+  userfolders,
+}: SideBarProps) => {
+  const [folderName, setfolderName] = useState('')
+
+  const onSave = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+  }
+
+  return (
+    <>
+      <h2 className='grid-title'>
+        <i className='fa fa-inbox'></i> Mailbox
+      </h2>
+      <Link to='/'>
+        <i className='fa fa-pencil'></i>&nbsp;&nbsp;NEW MESSAGE
+      </Link>
+      <hr />
+      <div>
+        <ul className='nav nav-pills nav-stacked' style={{ display: 'block' }}>
+          <li className='header'>Folders</li>
+          <li>
+            <Link to={'/folder/inbox'}>
+              <i className='fa fa-inbox' key={'inbox'}></i> Inbox ()
+            </Link>
+          </li>
+          <li>
+            <Link to={'/folder/starred'} key={'starred'}>
+              <i className='fa fa-star'></i> Starred ()
+            </Link>
+          </li>
+          <li>
+            <Link to={'/folder/important'} key={'important'}>
+              <i className='fa fa-bookmark'></i> Important ()
+            </Link>
+          </li>
+          <li>
+            <Link to={'/folder/sent'} key={'sent'}>
+              <i className='fa fa-mail-forward'></i> Sent ()
+            </Link>
+          </li>
+          <li>
+            <Link to={'/folder/spam'} key={'spam'}>
+              <i className='fa fa-folder'></i> Spam ()
+            </Link>
+          </li>
+          {userfolders.length > 0 &&
+            userfolders.map((e) => (
+              <li key={e.name}>
+                <Link to={'/folder/' + e.name} key={e.name}>
+                  <i className='fa fa-folder'></i>
+                  {' ' + e.name + ' (' + e.messagesId.length + ')'}
+                </Link>
+              </li>
+            ))}
+          <div className='input-group input-group-sm mb-3'>
+            <input
+              type='text'
+              className='form-control'
+              aria-label='Small'
+              aria-describedby='inputGroup-sizing-sm'
+              value={folderName}
+              onChange={(e) => setfolderName(e.target.value)}
+            />
+            <div
+              className='input-group input-group-sm mb-3'
+              style={{ display: 'grid' }}
+            >
+              <button
+                className='btn btn-primary btn-sm'
+                type='button'
+                onClick={onSave}
+              >
+                Button
+              </button>
+            </div>
+          </div>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+const mapStateToProps = (state: RootState) => ({
+  userfolders: state.user.folders,
+})
+
+const mapDispatchToProps = {}
+
+export const Sidebar = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(SidebarComponent)
